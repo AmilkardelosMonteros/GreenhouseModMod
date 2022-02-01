39198cb8e01f23ecf8b8c08cdab6ee0a2d2e65e6
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-01-31 22:58:42 -0600

    Instancia ddpg

---
+++ try_ddpg.py
@@ -0,0 +1,12 @@
+from ddpg.ddpg import DDPGagent as Agent
+import numpy as np
+from parameters.parameters_ddpg import PARAMS_DDPG
+
+class env:
+    def __init__(self):
+        self.observation_space = np.zeros((10,10))
+        self.action_space = np.zeros((10,10))
+
+env1 = env()
+agent = Agent(env1,PARAMS_DDPG)
+
