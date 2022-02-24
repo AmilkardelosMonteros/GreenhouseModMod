4f6017066e7b139cc41fa0900a3c74fb32313564
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2022-02-23 18:25:18 -0600

    inherited from v01

---
+++ try_weather_module.py
@@ -0,0 +1,12 @@
+from ModMod import  ReadModule
+from util import Loader
+loader = Loader("Creating weather module").start()
+MeteoModule = ReadModule('weather_data/pandas_to_excel.xlsx', t_conv_shift=0.0, t_conv=1)  # t_conv=1/(60*24) es para pasar el tiempo de minutos (como queda después de la lectura de la base) a días 
+loader.stop()
+
+'''
+vid = 'I2'
+s = 0
+traw = MeteoModule.data[MeteoModule.input_vars.loc[vid,'Sheet']].loc[MeteoModule.input_vars.loc[vid,'time_index']+s,MeteoModule.input_vars.loc[vid,'Time_column']]
+output = MeteoModule.tconv*(MeteoModule.input_vars.loc[vid,'Time_conv']*(traw - MeteoModule.input_vars.loc[vid,'Time_conv_shift']) - MeteoModule.tconv_a)
+'''
