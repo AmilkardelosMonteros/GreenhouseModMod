39c2ad8ad69f5da4623c64815dbce3e4adce919e
Sergio Fernández <sergfer26@Sergios-MacBook-Pro.local>
2022-02-23 18:22:39 -0600

    must be in climate

---
+++ greenhouse/module_control.py
@@ -0,0 +1,18 @@
+from ModMod import Module
+from scipy.stats import norm
+
+class ModuleControl(Module):
+    def __init__(self, Dt=1, **kwargs):
+        """Models one part of the process, uses the shared variables
+           from Director.
+           Dt=0.1, default Time steping of module
+        """
+        super().__init__(Dt)  # Time steping of module
+        # Always, use the super class __init__, theare are several otjer initializations
+        # Module specific constructors, add RHS's
+        for key, value in kwargs.items():
+            self.AddStateRHS(key, value)
+        # print("State Variables for this module:", self.S_RHS_ids)
+
+    def Advance(self, t1):
+        return 1
