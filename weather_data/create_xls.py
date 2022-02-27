4fee45943ecaa9baa37cac9d231c6144e7ba6f93
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-02-27 14:33:06 -0600

    edited time_swift, now is considers minutes

--- weather_data/create_xls.py
+++ weather_data/create_xls.py
@@ -15,7 +15,7 @@ def create_inputs_table():
     columns = ['Var', 'Description','Units','Sheet'	,'Column','Column_units','Column_conv_shift','Column_conv',	'Time_column',	'Time_column_units','Time_conv_shift','Time_conv']
     data    = pd.DataFrame(columns = columns)
     for i,var in enumerate(active_vars):
-        list_tem    = [Vars[var].new_name,Vars[var].new_name + Vars[var].obs,Vars[var].units,'Meteo',var,Vars[var].units,0,1,'Time_Stamp',pd.NA,2667452400,60*(1/3600)]
+        list_tem    = [Vars[var].new_name,Vars[var].new_name + Vars[var].obs,Vars[var].units,'Meteo',var,Vars[var].units,0,1,'Time_Stamp',pd.NA,2667452400,1]
         data.loc[i] = list_tem
     return data
 
