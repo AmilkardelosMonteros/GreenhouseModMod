2da028b1bb0c339e3029c649b6cb93fdc3589079
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-05 00:01:10 -0600

    Graficas de las variables de estado

---
+++ utils/graphics.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def create_images(model,list_var=None,PATH=None):
+    if PATH == None: SHOW = True
+    if list_var == None: list_var = list(model.Vars.keys())
+    for name in list_var:
+        if model.Vars[name].typ == 'State':
+            try:
+                x = model.OutVar(name)
+                title = model.Vars[name].prn
+                units = model.Vars[name].units
+                plt.plot(x, ms='4',markevery=60, marker='.')
+                plt.ylabel(units)
+                plt.xlabel('Time')
+                plt.title(title)
+                if SHOW:
+                    plt.show()
+                    plt.close()
+                else:
+                    plt.savefig(PATH + '/images/'+name+'.png')
+                    plt.close()
+            except:
+                print('La variable ', name,'tiene algo raro')
