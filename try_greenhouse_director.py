d55e2c92d089b8badc80837e6488e04ed5145298
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-09 19:15:11 -0600

    cambios

--- try_greenhouse_director.py
+++ try_greenhouse_director.py
@@ -75,8 +75,124 @@ cost_list = [Qgas_rhs_ins, Qh2o_rhs_ins, Qco2_rhs_ins]#, Qelec_rhs_ins]
 #dir_climate.MergeVarsFromRHSs(cost_list, call=__name__)
 symb_time_units = C1_rhs_ins.CheckSymbTimeUnits(C1_rhs_ins)
 # Genetare the director
+from sympy import symbols
+from ModMod import StateRHS
+s, mol_CO2, mol_air, mol_phot, m, d, C, g, mol_O2, pa, ppm = symbols('s mol_CO2 mol_air mol_phot m d C g mol_O2 pa ppm')
+mu_mol_CO2 = 1e-6 * mol_CO2
+mu_mol_phot = 1e-6 * mol_phot
+mu_mol_O2 = 1e-6 * mol_O2
+mg = 1e-3*g
+## Growth model
 
-""""""
+n_f, n_p, MJ = symbols('n_f n_p MJ') # number of fruits, number of plants
+## Climate model
+# C1
+mt, mg, m, C, s, W, mg_CO2, Joule, g, mol_CH2O = symbols('mt mg m C s W mg_CO2 Joule g mol_CH2O')
+# V1
+mt, mg, m, C, s, W, mg_CO2, Joule, Pa, kg_water, kg, K, ppm, kmol, kg_air, kg_vapour = symbols('mt mg m C s W mg_CO2 Joule Pa kg_water kg K ppm kmol kg_air kg_vapour')
+# T1
+mt, mg, m, C, s, W, mg_CO2, Joule, Pa, kg_water, kg, K, ppm = symbols('mt mg m C s W mg_CO2 Joule Pa kg_water kg K ppm')
+# T2
+mt, mg, m, C, s, W, mg_CO2, Joule, Pa, kg_water, kg, K, ppm, m_cover, kg_air = symbols('mt mg m C s W mg_CO2 Joule Pa kg_water kg K ppm m_cover kg_air')
+
+
+
+#################################################################
+############ RHS modelo de crecimiento ##########################
+#################################################################    
+class Q_rhs(StateRHS):
+    """
+    Q is the weight of all fruits for plant
+    """
+    def __init__( self, theta_p):
+        """Define a RHS, ***this an assigment RHS***, V1 = h2(...), NO ODE."""
+        ### uses the super class __init__
+        super().__init__()
+        
+        ### Define variables here.  Each fruit will have repeated variables.
+        ### Later some will be shared and the Local variable swill be exclusive
+        ### of each fruit.
+        
+        self.SetSymbTimeUnits(d) # days
+
+        ### State variables coming from the climate model
+        self.AddVar( typ='State', varid='T2', prn=r'$T_2$',\
+                    desc="Greenhouse air temperature", units= C , val=20) 
+        
+        #self.AddVar( typ='State', varid='PAR', prn=r'$PAR$',\
+         #   desc="PAR radiation", units=mu_mol_phot * (m**-2) * d**-1 , val=300.00)
+        self.AddVar( typ='State', varid='I2', prn=r'$I_2$',\
+                    desc="External global radiation", units= W * m**-2 , val=100) # It's takes as the PAR 
+        
+        
+        ### Local variables, separate for each plant
+        self.AddVarLocal( typ='State', varid='A', prn=r'$A$',\
+           desc="Assimilation rate", units= g * (m**-2), val=0)
+        
+        self.AddVarLocal( typ='StatePartial', varid='Q', prn=r'$Q$',\
+           desc="Weight of all fruits for plant", units= g, val=0.0)
+        
+        self.AddVarLocal( typ='StatePartial', varid='m_k', prn=r'$m_k$',\
+           desc="Simulation number of fruits harvested for plant", units= n_f, val=0.0)
+        
+        self.AddVarLocal( typ='StatePartial', varid='n_k', prn=r'$n_k$',\
+           desc="Number of fruits harvested for plant", units= n_f, val=0)
+
+        self.AddVarLocal( typ='StatePartial', varid='h_k', prn=r'$h_k$',\
+           desc="Weight of all harvested fruits for plant", units= g, val=0.0)
+
+        self.AddVarLocal( typ='StatePartial', varid='Q_h', prn=r'$H$',\
+           desc="Accumulated weight of all harvested fruits for plant", units= g, val=0.0)
+
+        self.AddVarLocal( typ='StatePartial', varid='Y_sum', prn=r'$Y_{sum}$',\
+           desc="Sum of all potentail growths", units= g/d**2, val=0.0)
+
+
+        ### Canstants, shared by all plants.  Shared Cnts cannot be local
+        self.AddVar( typ='Cnts', varid='k1_TF', prn=r'$k1_TF$',\
+           desc="Aux in function TF", units= MJ * m**-2 * d**-1, val=300.0)
+
+        self.AddVar( typ='Cnts', varid='k2_TF', prn=r'$k2_TF$',\
+           desc="Aux in function TF", units= C * d**-1, val=1.0)
+
+        self.AddVarLocal( typ='Cnts', varid='k3_TF', prn=r'$k3_TF$',\
+           desc="Aux in function TF", units= n_f * C**-1, val=1.0)
+
+        self.AddVarLocal( typ='Cnts', varid='dw_ef', prn=r'$dw_{efficacy}$',\
+           desc="Constant in t_wg for fruits", units= 1, val=1.3)
+        
+        self.AddVarLocal( typ='Cnts', varid='dw_ef_veg', prn=r'$dw_{efficacy}$',\
+           desc="Constant in t_wg for vegetative part", units= 1, val=1.15)
+
+        self.AddVarLocal( typ='Cnts', varid='a_ef', prn=r'$a_{efficacy}$',\
+           desc="Matching constant in remaining assimilates", units= 1/m**2, val=1.0)
+
+        self.AddVarLocal( typ='Cnts', varid='C_t', prn=r'$C_t$',\
+           desc="Constant in Y_pot", units= C * d, val=131.0)
+
+        self.AddVarLocal( typ='Cnts', varid='B', prn=r'$B$',\
+           desc="Constant in Y_pot", units= (C * d)**-1, val=0.017)
+
+        self.AddVarLocal( typ='Cnts', varid='D', prn=r'$D$',\
+           desc="Constant in Y_pot", units= 1, val=0.011)
+
+        self.AddVarLocal( typ='Cnts', varid='M', prn=r'$M$',\
+           desc="Constant in Y_pot", units= g, val=60.7)
+        
+        self.AddVarLocal( typ='Cnts', varid='a', prn=r'$a$',\
+           desc="Constant in Y_pot_veg", units= 1, val=theta_p[1])
+        
+        self.AddVarLocal( typ='Cnts', varid='b', prn=r'$b$',\
+           desc="Constant in Y_pot_veg", units= 1, val=theta_p[2])
+
+
+    def RHS( self, Dt):
+        """RHS( Dt ) = 
+           
+           ************* IN ASSIGMENT RHSs WE DON'T NEED TO CALL STATE VARS WITH self.Vk ******************
+        """
+        ### The assigment is the total weight of the fuits
+        return self.V('Q')
 
 def PlantDirector( beta, return_Q_rhs_ins=False):
     """Build a Director to hold a Plant, with beta PAR parameter."""
@@ -84,7 +200,8 @@ def PlantDirector( beta, return_Q_rhs_ins=False):
     Dir = Director( t0=0.0, time_unit="", Vars={}, Modules={} )
     ## Create instances of RHS
     Ci_rhs_ins = Ci_rhs(constant_crop)
-    Q_rhs_ins = Q_rhs(constant_crop)
+    #Q_rhs_ins = Q_rhs(constant_crop)
+    Q_rhs_ins = Q_rhs(theta_p)
     ## Add time information to the Director
     Dir.AddTimeUnit( Ci_rhs_ins.GetTimeUnits())
     Dir.AddTimeUnit( Q_rhs_ins.GetTimeUnits())
@@ -113,6 +230,7 @@ director.AddDirectorAsModule('Climate', dir_climate)
 director.PlantList = []
 for p, beta in enumerate(beta_list):
     ### Make and instance of a Plant
+    breakpoint()
     Dir = PlantDirector(beta=beta)
     
     ### Merge all ***global*** vars from plant
