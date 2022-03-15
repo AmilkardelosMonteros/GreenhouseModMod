81775376dded5c0ff007874bf379acd042f74095
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-03-14 22:12:49 -0600

    added parameter module for create_images

--- utils/graphics.py
+++ utils/graphics.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 
-def create_images(model,list_var=None,PATH=None):
+def create_images(model, module, list_var=None,PATH=None):
     SHOW = False
     if PATH == None: SHOW = True
-    if list_var == None: list_var = list(model.Vars.keys())
+    if list_var == None: list_var = list(model.Modules[module].Vars.keys())
     for name in list_var:
         if model.Vars[name].typ == 'State':
             try:
-                x = model.OutVar(name)
+                x = model.Modules[module].OutVar(name)
+                t = range(len(x))
                 print('Graficando',name)
                 title = model.Vars[name].prn
                 units = model.Vars[name].units
-                plt.plot(x, ms='4',markevery=60, marker='.')
+                plt.plot(t, x, ms='4',markevery=60, marker='.')
                 plt.ylabel(units)
                 plt.xlabel('Time')
                 plt.title(title)
