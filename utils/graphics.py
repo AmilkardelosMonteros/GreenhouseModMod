c445cf52681552169b65aad100501fa2edffc03f
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-23 22:58:06 -0600

    Se agrego la descripcion de algunos flujos

--- utils/graphics.py
+++ utils/graphics.py
@@ -4,23 +4,25 @@ def create_images(model, module, list_var=None,PATH=None):
     SHOW = False
     if PATH == None: SHOW = True
     if list_var == None: list_var = list(model.Vars.keys())
-    for name in list_var:
+    for j,name in enumerate(list_var):
         if model.Vars[name].typ == 'State':
             try:
                 x = model.OutVar(name)
                 t = range(len(x))
                 print('Graficando',name)
                 title = model.Vars[name].prn
+                subtitle = model.Vars[name].desc
                 units = model.Vars[name].units
-                plt.plot(t, x, ms='4',markevery=60, marker='.')
+                plt.plot(t, x, ms='4',alpha = 0.7)
                 plt.ylabel(units)
                 plt.xlabel('Time')
                 plt.title(title)
+                plt.suptitle(subtitle)
                 if SHOW:
                     plt.show()
                     plt.close()
                 else:
-                    plt.savefig(PATH + '/images/'+name+'.png')
+                    plt.savefig(PATH + '/images/'+name+'_'+str(j)+'_.png')
                     plt.close()
             except:
                 print('La variable ', name,'tiene algo raro')
