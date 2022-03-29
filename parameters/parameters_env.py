1cc5da1dc92996ad11937341b7db099bbd5e0c4f
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-28 21:34:00 -0600

    Se agrego n a los parametros pero aun no se usa

--- parameters/parameters_env.py
+++ parameters/parameters_env.py
@@ -1,17 +1,14 @@
 import json
 minutos = 60
-PARAMS_ENV = {'STEP': minutos/(24*60), \
-              'TIME_MAX': 90, #Dias simulados\ 
-              'FRECUENCY': 60, 
-              'SEASON':1, \
-              'MINUTOS':minutos}
+
+PARAMS_ENV = {'n':10}
 
 #Para entrenamiento SEASON puede ser 1,2 o 'RANDOM'
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
 PARAMS_TRAIN = {'EPISODES': 1000, \
-                'STEPS': int(PARAMS_ENV['TIME_MAX']/PARAMS_ENV['STEP']), \
+                'STEPS':PARAMS_ENV['n'], \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
                 'SERVER':True, \
@@ -25,15 +22,4 @@ PARAMS_SIM = {'anio':2017,\
             'hora': 1
             }
             
-CONTROLS = {'u_1': 1, 
-            'u_2': 1, 
-            'u_3': 1, 
-            'u_4': 0,   
-            'u_5': 0, #No hace nada por los parametros
-            'u_6': 0, 
-            'u_7': 1, 
-            'u_8': 1,  
-            'u_9': 1, 
-            'u_10': 1,
-            'u_11': 1, 
-            }
+
