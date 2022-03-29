15c2030c55d9f08017f59f5c61b016cf8fdd6a46
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-28 21:34:33 -0600

    Se agrego decay_period = n

--- parameters/parameters_utils.py
+++ parameters/parameters_utils.py
@@ -1,3 +1,13 @@
 from .parameters_env import PARAMS_ENV
+from .parameters_ddpg import CONTROLS
 
-PARAMS_UTILS = {'mu':0.0, 'theta': 0.00, 'max_sigma':0.01, 'min_sigma': 0.0, 'decay_period':int(PARAMS_ENV['TIME_MAX']*PARAMS_ENV['STEP']**-1)}
+PARAMS_UTILS = {'mu':0.0, 
+                'theta': 0.00, 
+                'max_sigma':0.01, 
+                'min_sigma': 0.0, 
+                'decay_period':PARAMS_ENV['n'],
+                'low':0,
+                'high':1,
+                'dim':sum(CONTROLS.values())
+                }
+
