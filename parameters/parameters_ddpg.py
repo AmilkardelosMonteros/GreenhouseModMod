2297c8ce7274e9b2658cf29dbb5392555776f4fe
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-03-29 23:14:36 -0600

    se apagaron los controles

--- parameters/parameters_ddpg.py
+++ parameters/parameters_ddpg.py
@@ -2,17 +2,17 @@ STATE_VARIABLES = ['T1','T2','C1','V1']
 INPUTS          = ['I2','I5','I8','I11']
 VARS_OF_DIR     = STATE_VARIABLES + INPUTS
 
-CONTROLS = {'U1':  True, 
-            'U2':  True, 
-            'U3':  True, 
+CONTROLS = {'U1':  False, 
+            'U2':  False, 
+            'U3':  False, 
             'U4':  False,   
             'U5':  False, #No hace nada por los parametros
             'U6':  False, 
-            'U7':  True, 
-            'U8':  True,  
-            'U9':  True, 
-            'U10': True,
-            'U11': True, 
+            'U7':  False, 
+            'U8':  False,  
+            'U9':  False, 
+            'U10': False,
+            'U11': False, 
             }
 
 PARAMS_DDPG = {'hidden_sizes': [64,64,64], 
@@ -20,5 +20,6 @@ PARAMS_DDPG = {'hidden_sizes': [64,64,64],
                 'critic_learning_rate': 1e-3, 
                 'gamma':0.98, 'tau':0.125, 
                 'max_memory_size':int(1e5),
+                'batch_size': 32,
                 'vars':VARS_OF_DIR,
                 'controls':CONTROLS}
