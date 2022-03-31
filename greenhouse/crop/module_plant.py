fc61cffa4548f03cdea7476df9a766bdcb230f40
acapellakort <capella@im.unam.mx>
2022-03-30 19:27:10 -0600

    Actualizacion Advance a no aleatorio
    Eliminamos el loop de steps y la variable tt. Se creo una variable hk para acumular el peso de los frutos. Eliminamos la aleatoriedad del peso de los frutos. Los frutos ya no se calculan proporcionales a su numero.

--- greenhouse/crop/module_plant.py
+++ greenhouse/crop/module_plant.py
@@ -45,89 +45,90 @@ class Plant(Module):
         ### This creates a set of times knots, that terminates in t1 with
         ### a Deltat <= self.Dt
         if t1 % 86400 == 0:
-            tt = append( arange( self.t(), t1, step=self.Dt_g), [t1])
+            #tt = append( arange( self.t(), t1, step=self.Dt_g), [t1])  ### <---- no se deberia de usar
             breakpoint()
-            Dt= (tt[1] -tt[0]) / (24*60*60) #En dias 
+            Dt= 1 # (tt[1] -tt[0]) / (24*60*60) #En dias   ####<-------- corregir
             #print(tt)
-            steps = len(tt)
-            for i in range( 1, steps):
+            #steps = len(1)
+            #for i in range( 1, steps):      ####<---------------- corregir este for no va 
                 ### Check if a fruit is ready to be harvest
-                harvest = []
-                nfk = 0
-                for h, fruit in enumerate(self.fruits): # h is the indice and fruit is the object
-                    if (fruit[0] > 275 or fruit[1]>360): # It is harvested when a fruit reaches a thermic age of 275 °C d or if the fruit's weigth is greater than 360 g
-                        harvest += [h]
-                        self.n_fruits -= 1 # number fruits in crop
-                        nfk += 1 # number fruits harvested in this moment
-                [self.fruits.pop(i) for i in harvest]# Harvested fruits are removed from the list
-                self.V_Set( 'm_k', nfk) # Número de frutos que calcula el simulador
-                sc = (self.V('m_k') + 1e-6 ) / 16 # Este es el parámetro de escala de la distribución gamma, igual a beta**{-1}
-                nk = gamma.rvs(a=16, scale=sc) # Número de frutos considerando aleatoriedad
-                sigma_F = 1.
-                hk = 380*nk + norm.rvs( scale=sqrt(nk)*sigma_F ) # Peso de frutos considerando aleatoriedad
-                self.V_Set( 'n_k', nk)
-                self.V_Set( 'h_k', hk)
-                w = self.V( 'Q_h') + self.V('h_k') # accumulated weight fruits harvested
-                self.V_Set( 'Q_h', w)
-                self.n_fruits_h += self.V('n_k') # accumulated number fruits harvested
-
-                ### With the Floration Rate, create new fruits
-                PA_mean_i = self.beta * self.V('I2')
-                self.new_fruit += TF( k1_TF=self.V('k1_TF'), k2_TF=self.V('k2_TF'), k3_TF=self.V('k3_TF'),\
-                        PA_mean=PA_mean_i, T_mean=self.V('T1'), Dt=Dt)
-                new_fruit_n = self.new_fruit 
-                if new_fruit_n >= 1:
-                    #nw = new_fruit_n
-                    nw = int(floor(self.new_fruit))
-                    for nf in range(nw):
-                        ### Add new fruit
-                        self.fruits += [[ 0.0, 0.0, 0.0, 0.0]] 
-                        ### also the growth potential, as an auxiliar for calculations
-                        self.n_fruits += 1 
-                    ### Leave the rational part of new_fruit
-                    self.new_fruit = max( 0, self.new_fruit - nw)
-
-                ### Update thermic age of all fruits
-                for fruit in self.fruits:
-                    fruit[0] += ( max( 0 , self.V('T2') - 10 ) )* Dt## Thermic age never decreases
-
-                ### Update growth potencial for vegetative part
-                self.veget[1] = self.V('a') + self.V('b')*self.V('T2') 
-                ### Update Growth potential and Michaelis-Menten constants of all fruits
-                tmp = 0.0
-                tmp1 = self.veget[1] / self.V('A') # start with the growth potencial of vegetative part
-                for fruit in self.fruits:
-                    x = fruit[0] ## Thermic age
-                    ### Michaelis-Menten constants 
-                    if x <= self.V('C_t') :
-                        fruit[3] = 0.05*tmp*(self.V('C_t') - x) / self.V('C_t')
-                    ### Growth potential
-                    fruit[2] = clip( Y_pot( k2_TF=self.V('k2_TF'), C_t=self.V('C_t'),\
-                         B=self.V('B'), D=self.V('D'), M=self.V('M'), X=x, T_mean=self.V('T2')),\
-                         a_min=0, a_max=exp(300))
-                    tmp += fruit[2]
-                    tmp1 += fruit[2] / ( fruit[3] + self.V('A') )
-                #self.V_Set( 'Y_sum', tmp)
-
-                ### Update weight of vegetative part
-                f_wg_veg =  self.veget[1] / ( self.V('A') * tmp1  ) # The sink strentgh of vegetative part
-                self.veget[0] += t_wg( dw_ef=self.V('dw_ef_veg'), A=self.V('A'), f_wg=f_wg_veg) * Dt
-                #### Update weight of all fruits
-                tmp2 = 0.0
-                for fruit in self.fruits:
-                    f_wg =  fruit[2] / ( ( fruit[3] + self.V('A') ) * tmp1 ) # The sink strentgh of the fruit
-                    dwh = t_wg( dw_ef=self.V('dw_ef'), A=self.V('A'), f_wg=f_wg) * Dt # dry weight
-                    pdw = 0.023 # percentage of dry weight
-                    fruit[1] += dwh / pdw # Fresh weight  
-                    tmp2 += fruit[1] #Total weight
-
-                #### Update assimilation rate after distribution
-                m = ( f_wg_veg / self.V('dw_ef_veg') ) + ( (1 - f_wg_veg ) / self.V('dw_ef') )
-                As = self.V('A')*( 1 - self.V('a_ef')*Dt*m ) # A = A - ( Total weigth of fruits and vegetative part )
-                self.V_Set('A', As )  
-
-                #### Total weight of the fruits
-                self.V_Set( 'Q', tmp2)
-                #### Advance of the RHS
-                self.AdvanceAssigment(t1) # Set Q
+            harvest = []
+            nfk = 0
+            '''Esta es una nueva variable hk'''
+            hk = 0 
+            for h, fruit in enumerate(self.fruits): # h is the indice and fruit is the object
+                if (fruit[0] > 275 or fruit[1]>360): # It is harvested when a fruit reaches a thermic age of 275 °C d or if the fruit's weigth is greater than 360 g
+                    harvest += [h]
+                    self.n_fruits -= 1 # number fruits in crop
+                    nfk += 1 # number fruits harvested in this moment
+                    ''' Esta linea cambió'''
+                    hk += fruit[1] # weight of harvested fruits in this moment
+            [self.fruits.pop(i) for i in harvest]# Harvested fruits are removed from the list
+            ''' 
+            self.V_Set( 'm_k', nfk) # Número de frutos que calcula el simulador
+            sc = (self.V('m_k') + 1e-6 ) / 16 # Este es el parámetro de escala de la distribución gamma, igual a beta**{-1}
+            nk = gamma.rvs(a=16, scale=sc) # Número de frutos considerando aleatoriedad
+            sigma_F = 1.
+            hk = 380*nk + norm.rvs( scale=sqrt(nk)*sigma_F ) # Peso de frutos considerando aleatoriedad
+            '''
+            self.V_Set( 'n_k', nfk)
+            self.V_Set( 'h_k', hk)
+            w = self.V( 'Q_h') + self.V('h_k') # accumulated weight fruits harvested
+            self.V_Set( 'Q_h', w)
+            self.n_fruits_h += self.V('n_k') # accumulated number fruits harvested
+            ### With the Floration Rate, create new fruits
+            PA_mean_i = self.beta * self.V('I2')
+            self.new_fruit += TF( k1_TF=self.V('k1_TF'), k2_TF=self.V('k2_TF'), k3_TF=self.V('k3_TF'),\
+                    PA_mean=PA_mean_i, T_mean=self.V('T1'), Dt=Dt)
+            new_fruit_n = self.new_fruit 
+            if new_fruit_n >= 1:
+                #nw = new_fruit_n
+                nw = int(floor(self.new_fruit))
+                for nf in range(nw):
+                    ### Add new fruit
+                    self.fruits += [[ 0.0, 0.0, 0.0, 0.0]] 
+                    ### also the growth potential, as an auxiliar for calculations
+                    self.n_fruits += 1 
+                ### Leave the rational part of new_fruit
+                self.new_fruit = max( 0, self.new_fruit - nw)
+            ### Update thermic age of all fruits
+            for fruit in self.fruits:
+                fruit[0] += ( max( 0 , self.V('T2') - 10 ) )* Dt## Thermic age never decreases
+            ### Update growth potencial for vegetative part
+            self.veget[1] = self.V('a') + self.V('b')*self.V('T2') 
+            ### Update Growth potential and Michaelis-Menten constants of all fruits
+            tmp = 0.0
+            tmp1 = self.veget[1] / self.V('A') # start with the growth potencial of vegetative part
+            for fruit in self.fruits:
+                x = fruit[0] ## Thermic age
+                ### Michaelis-Menten constants 
+                if x <= self.V('C_t') :
+                    fruit[3] = 0.05*tmp*(self.V('C_t') - x) / self.V('C_t')
+                ### Growth potential
+                fruit[2] = clip( Y_pot( k2_TF=self.V('k2_TF'), C_t=self.V('C_t'),\
+                     B=self.V('B'), D=self.V('D'), M=self.V('M'), X=x, T_mean=self.V('T2')),\
+                     a_min=0, a_max=exp(300))
+                tmp += fruit[2]
+                tmp1 += fruit[2] / ( fruit[3] + self.V('A') )
+            #self.V_Set( 'Y_sum', tmp)
+            ### Update weight of vegetative part
+            f_wg_veg =  self.veget[1] / ( self.V('A') * tmp1  ) # The sink strentgh of vegetative part
+            self.veget[0] += t_wg( dw_ef=self.V('dw_ef_veg'), A=self.V('A'), f_wg=f_wg_veg) * Dt
+            #### Update weight of all fruits
+            tmp2 = 0.0
+            for fruit in self.fruits:
+                f_wg =  fruit[2] / ( ( fruit[3] + self.V('A') ) * tmp1 ) # The sink strentgh of the fruit
+                dwh = t_wg( dw_ef=self.V('dw_ef'), A=self.V('A'), f_wg=f_wg) * Dt # dry weight
+                pdw = 0.023 # percentage of dry weight
+                fruit[1] += dwh / pdw # Fresh weight  
+                tmp2 += fruit[1] #Total weight
+            #### Update assimilation rate after distribution
+            m = ( f_wg_veg / self.V('dw_ef_veg') ) + ( (1 - f_wg_veg ) / self.V('dw_ef') )
+            As = self.V('A')*( 1 - self.V('a_ef')*Dt*m ) # A = A - ( Total weigth of fruits and vegetative part )
+            self.V_Set('A', As )  
+            # Revisar si 'As==0'? <--------------------------------
+            #### Total weight of the fruits
+            self.V_Set( 'Q', tmp2)
+            #### Advance of the RHS
+            self.AdvanceAssigment(t1) # Set Q
         return 1
