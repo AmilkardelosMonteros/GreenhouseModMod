0d9e2176408096b84161675506262393db198f35
acapellakort <capella@im.unam.mx>
2022-04-08 14:50:07 -0500

    Conflictos

--- greenhouse/crop/module_photo.py
+++ greenhouse/crop/module_photo.py
@@ -33,7 +33,6 @@ class PhotoModule(Module):
 
     def Advance( self, t1):
         ## Actualización de la tasa de asimilación
-<<<<<<< HEAD
         tt = append( arange(self.t(), t1, step=self.Dt), [t1])
         n = len(tt)  ### Number of time steps
         for i in range( 1, n): 
@@ -48,8 +47,8 @@ class PhotoModule(Module):
             Gamma_st1 = Gamma_st( T_f=T1 )
             I_21 = I_2( I = I2, f=self.V('f'), ab=self.V('ab') )
             J1 = J( I_2=I_21, J_max=self.V('J_max'), theta=self.V('theta') )
-            A_R1 = A_R( O_a=self.V('O_a'), tau=tau1, C_i=self.V('Ci'), V_cmax=V_cmax1, Gamma_st=Gamma_st1, K_C=K_C1, K_O=K_O1)
-            A_f1 = A_f( C_i=self.V('Ci'), Gamma_st=Gamma_st1, J=J1, k_JV=self.V('k_JV') )
+            A_R1 = A_R( O_a=self.V('O_a'), tau=tau1, C_ippm=self.V('Ci'), V_cmax=V_cmax1, Gamma_st=Gamma_st1, K_C=K_C1, K_O=K_O1)
+            A_f1 = A_f( C_ippm=self.V('Ci'), Gamma_st=Gamma_st1, J=J1, k_JV=self.V('k_JV') )
             A_acum1 = A_acum( V_cmax=V_cmax1 )
             A1 = A( A_R=A_R1, A_f=A_f1, A_acum=A_acum1, R_d=R_d1, fc=self.V('fc') ) 
             self.V_Set('A', A1)
@@ -60,26 +59,4 @@ class PhotoModule(Module):
             ## Avance del RHS
             self.AdvanceRungeKutta(t1=tt[i], t0=tt[i-1]) 
 
-=======
-        ind_pho = self.V('ind_pho') 
-        T1 = self.V_GetRec('T1', ind_get=ind_pho)
-        I2 = self.V_GetRec('I2', ind_get=ind_pho)
-        V_cmax1 = V_cmax( T_f=T1, V_cmax25=self.V('V_cmax25'), Q10_Vcmax=self.V('Q10_Vcmax'), k_T=self.V('k_T') )
-        R_d1 = R_d( V_cmax=V_cmax1 )
-        tau1 = tau( T_f=T1, tau_25=self.V('tau_25'), Q10_tau=self.V('Q10_tau'), k_T=self.V('k_T') )
-        K_C1 = K_C( T_f=T1, K_C25=self.V('K_C25'), Q10_KC=self.V('Q10_KC'), k_T=self.V('k_T') )
-        K_O1 = K_O( T_f=T1, K_O25=self.V('K_O25'), Q10_KO=self.V('Q10_KO'), k_T=self.V('k_T') )
-        Gamma_st1 = Gamma_st( T_f=T1 )
-        I_21 = I_2( I = I2, f=self.V('f'), ab=self.V('ab') )
-        J1 = J( I_2=I_21, J_max=self.V('J_max'), theta=self.V('theta') )
-        A_R1 = A_R( O_a=self.V('O_a'), tau=tau1, C_ippm=self.V('Ci'), V_cmax=V_cmax1, Gamma_st=Gamma_st1, K_C=K_C1, K_O=K_O1)
-        A_f1 = A_f( C_ippm=self.V('Ci'), Gamma_st=Gamma_st1, J=J1)
-        A_acum1 = A_acum( V_cmax=V_cmax1 )
-        A1 = abs( A( A_R=A_R1, A_f=A_f1, A_acum=A_acum1, R_d=R_d1, fc=self.V('fc') ) )
-        self.V_Set('A', A1)
-        #
-        self.V_Set('ind_pho', ind_pho + 1) # Después de que se calcularon los asimilados, se actualiza el valor del indice auxiliar pues se avanza un minuto
-        ## Avance del RHS
-        self.AdvanceRungeKutta(t1) 
->>>>>>> 5630a6c (Correccion para que la fotosintesis use el CO2 intracelular)
         return 1
