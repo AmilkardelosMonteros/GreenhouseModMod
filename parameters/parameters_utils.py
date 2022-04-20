ab65cc6f6c1a846f5c0cb3b6d6054e2594e1abbb
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-04-20 12:24:12 -0500

    Se agreglo bug de los controles

--- parameters/parameters_utils.py
+++ parameters/parameters_utils.py
@@ -1,13 +1,14 @@
 from .parameters_env import PARAMS_ENV
 from .parameters_ddpg import CONTROLS
 
+
 PARAMS_UTILS = {'mu':0.0, 
                 'theta': 0.00, 
-                'max_sigma':0.01, 
+                'max_sigma':0.1, 
                 'min_sigma': 0.0, 
                 'decay_period':PARAMS_ENV['n'],
                 'low':0,
                 'high':1,
-                'dim':sum(CONTROLS.values())
+                'dim':len(CONTROLS.values())
                 }
 
