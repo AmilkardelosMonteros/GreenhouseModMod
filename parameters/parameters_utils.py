8e9290b03fa79a69f70cc5ccaf4cf077b4cc1a0d
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-04-26 18:41:46 -0500

    Se disminuyo la volatilidad del proceso

--- parameters/parameters_utils.py
+++ parameters/parameters_utils.py
@@ -4,7 +4,7 @@ from .parameters_ddpg import CONTROLS
 
 PARAMS_UTILS = {'mu':0.0, 
                 'theta': 0.00, 
-                'max_sigma':0.1, 
+                'max_sigma':0.01, 
                 'min_sigma': 0.0, 
                 'decay_period':PARAMS_ENV['n'],
                 'low':0,
