299ef8667e6969fd0989a3210c3c588fc4f75370
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-04-26 18:43:07 -0500

    Se agrego la forma corta para guardar

--- utils/for_simulation.py
+++ utils/for_simulation.py
@@ -1,3 +1,6 @@
+from importlib.resources import path
+
+
 def set_train(dir):
     dir.Modules['Climate'].Modules['ModuleClimate'].train    = True
     dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = True
@@ -5,3 +8,10 @@ def set_train(dir):
 def set_simulation(dir):
     dir.Modules['Climate'].Modules['ModuleClimate'].train    = False
     dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = False
+
+def save_nets(dir,PATH,i):
+    print('Guardando red' + str(i))
+    dir.Modules['Climate'].Modules['ModuleClimate'].agent.save(path = PATH, name = str(i))
+
+def set_index(dir,index_):
+    dir.Modules['Climate'].Modules['ModuleMeteo'].shift_time = index_
