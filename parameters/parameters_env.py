98f2b589b051b5c044720b9f8a7513da588cb927
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-05-04 11:57:35 -0500

    Se simulan 4 episodios

--- parameters/parameters_env.py
+++ parameters/parameters_env.py
@@ -35,7 +35,7 @@ PARAMS_ENV = {'n': n}
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
-PARAMS_TRAIN = {'EPISODES': 5, \
+PARAMS_TRAIN = {'EPISODES': 4, \
                 'STEPS':PARAMS_ENV['n'], \
                 'BATCH_SIZE': 128, \
                 'SHOW': False, \
