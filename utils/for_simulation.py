cf8fc5abe388100ad8cf5aeabd2a85dea32ed50e
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-05-04 23:59:34 -0500

    puse agent como atributo del director principal

--- utils/for_simulation.py
+++ utils/for_simulation.py
@@ -2,16 +2,21 @@ from importlib.resources import path
 
 
 def set_train(dir):
-    dir.Modules['Climate'].Modules['ModuleClimate'].train    = True
-    dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = True
+    #dir.Modules['Climate'].Modules['ModuleClimate'].train    = True
+    #dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = True
+    dir.train    = True
+    dir.noise.on = True
 
 def set_simulation(dir):
-    dir.Modules['Climate'].Modules['ModuleClimate'].train    = False
-    dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = False
+    #dir.Modules['Climate'].Modules['ModuleClimate'].train    = False
+    #dir.Modules['Climate'].Modules['ModuleClimate'].noise.on = False
+    dir.true     = False
+    dir.noise.on = False
 
 def save_nets(dir,PATH,i):
     print('Guardando red' + str(i))
-    dir.Modules['Climate'].Modules['ModuleClimate'].agent.save(path = PATH, name = str(i))
+    #dir.Modules['Climate'].Modules['ModuleClimate'].agent.save(path = PATH, name = str(i))
+    dir.agent.save(path = PATH, name = str(i))
 
 def set_index(dir,index_):
     dir.Modules['Climate'].Modules['ModuleMeteo'].shift_time = index_
