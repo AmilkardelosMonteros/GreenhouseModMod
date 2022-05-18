de2fe5ec48ffa8827d7893a0302110d5d71683e9
Sergio Fernández <sfernandezm97@ciencias.unam.mx>
2022-05-18 18:48:31 -0500

    cambie dominio del ruido

--- parameters/parameters_utils.py
+++ parameters/parameters_utils.py
@@ -4,10 +4,10 @@ from .parameters_ddpg import CONTROLS
 
 PARAMS_UTILS = {'mu':0.0, 
                 'theta': 0.00, 
-                'max_sigma':0.001, 
+                'max_sigma':0.005, 
                 'min_sigma': 0.0, 
                 'decay_period':PARAMS_ENV['n'],
-                'low':0,
+                'low':-1,
                 'high':1,
                 'dim':len(CONTROLS.values())
                 }
