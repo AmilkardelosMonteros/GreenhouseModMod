77808e316df87a4ae2e47b44552c6b2bc797dd23
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-06-01 12:55:35 -0500

    Se disminuye sigma en el reset

--- try_noise.py
+++ try_noise.py
@@ -5,8 +5,8 @@ from parameters.parameters_utils import PARAMS_UTILS
 noise = OUNoise(PARAMS_UTILS)
 n=PARAMS_UTILS['decay_period']
 if __name__ == '__main__':
-    N_TEST = 10
+    N_TEST = 5
     for _ in range(N_TEST):
         test_noise(noise,n)
-        noise.max_sigma -= noise.max_sigma/(n*0.5)
         noise.reset()
+        print(noise.sigma)
