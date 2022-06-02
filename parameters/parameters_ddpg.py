f64accfd267f62e4d7faa9cc716331801a0c9547
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-06-02 15:03:30 -0500

    Cambios de temporada y capas

--- parameters/parameters_ddpg.py
+++ parameters/parameters_ddpg.py
@@ -17,7 +17,7 @@ CONTROLS = {'U1':  False, # Pantalla térmica
             'U12': False  # Lamparas 
             }
 
-PARAMS_DDPG = {'hidden_sizes': [64,64], 
+PARAMS_DDPG = {'hidden_sizes': [64,64,64], 
                 'actor_learning_rate': 1e-4, 
                 'critic_learning_rate': 1e-3, 
                 'gamma':0.98, 'tau':0.125, 
--- parameters/parameters_dir.py
+++ parameters/parameters_dir.py
@@ -1,3 +1,3 @@
 PARAMS_DIR = {'minutes': 5,
               'days':60,
-              'season':'1'}
+              'season':'2'}
