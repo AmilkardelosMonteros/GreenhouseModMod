0411bcc24e6d1338bb0bd89c9b8a7f1c16c784aa
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-06-06 17:28:38 -0500

    Se cambio el nombre de noise1 a noise

--- try_noise.py
+++ try_noise.py
@@ -2,13 +2,13 @@ from ddpg.utils import OUNoise
 from ddpg.utils import test_noise,test_reset
 from parameters.parameters_utils import PARAMS_UTILS
 
-noise1 = OUNoise(PARAMS_UTILS)
+noise  = OUNoise(PARAMS_UTILS)
 noise2 = OUNoise(PARAMS_UTILS)
 n=PARAMS_UTILS['decay_period']
 if __name__ == '__main__':
     N_TEST = 5
     print('Test sobre el reset')
-    test_reset(noise1)
+    test_reset(noise)
     for _ in range(N_TEST):
         test_noise(noise2,n)
         noise2.reset()
