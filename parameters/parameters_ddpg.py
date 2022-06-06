03153ac0a65c4bdd197aa769c1ba83e25b9ea7f5
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-06-06 17:13:48 -0500

    Se quitaron algunos controls

--- parameters/parameters_ddpg.py
+++ parameters/parameters_ddpg.py
@@ -4,16 +4,16 @@ CROP_VARIABLES  = ['h']
 VARS_OF_DIR     = STATE_VARIABLES + INPUTS + CROP_VARIABLES
 
 CONTROLS = {'U1':  False, # Pantalla térmica
-            'U2':  False, # Ventilador almoadilla
-            'U3':  True, # Enfriamiento mecanico
+            'U2':  True, # Ventilador almoadilla
+            'U3':  False, # Enfriamiento mecanico
             'U4':  False, # Calentador de aire  
             'U5':  False, # Sombreado externo (No hace nada por los parametros)
-            'U6':  False, # Respiraderos laterales
-            'U7':  False, # Ventilación forzada
+            'U6':  True, # Respiraderos laterales
+            'U7':  True, # Ventilación forzada
             'U8':  True, # Respiraderos del techo 
             'U9':  False, # Control sistema de niebla
-            'U10': True, # Fuente externa de C02
-            'U11': True, # Tuberia de calentamiento
+            'U10': False, # Fuente externa de C02
+            'U11': False, # Tuberia de calentamiento
             'U12': False  # Lamparas 
             }
 
