79a56d27b05a9cf57e680af1ae5360f82e9125b8
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2022-11-16 00:16:36 -0600

    Nuevo decorador

--- greenhouse/crop/Decorator.py
+++ greenhouse/crop/Decorator.py
@@ -2,23 +2,21 @@ import numpy as np
 
 def check(funtion):
     def check_nan(**kwargs):
-        if np.nan in kwargs.values():
-            print('Error en los parametros')
-            print(kwargs)  
-            return np.nan
+        result = funtion(*list(kwargs.values()))
+        if np.isnan(result):
+            print('La funcion {} tiene algo mal'.format(funtion))
+            print('Parametros:')
+            print(print(kwargs))
+            return result
         else:
-            result = funtion(*list(kwargs.values()))
-            if np.isnan(result):
-                print('La funcion {} tiene algo mal'.format(function))
-            else:
-                return result
+            return result
     return check_nan
 
 def main():
     @check
     def suma(x,y):
         return x+y
-    print(suma(x=np.nan,y = 1))
+    suma(x=np.nan,y = 1)
 
 if __name__ == '__main__':
     main()
