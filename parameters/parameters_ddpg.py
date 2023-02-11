f50cb72599495b6c415ca3faefb2b3942243ceba
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2023-02-10 22:47:19 -0600

    Se ajustaron los parametros para ser como U9U10

--- parameters/parameters_ddpg.py
+++ parameters/parameters_ddpg.py
@@ -3,13 +3,13 @@ INPUTS          = ['I2','I5','I8','I11']
 CROP_VARIABLES  = ['h'] 
 VARS_OF_DIR     = STATE_VARIABLES + INPUTS + CROP_VARIABLES
 
-CONTROLS = {'U1':  True, # Pantalla térmica
+CONTROLS = {'U1':  False, # Pantalla térmica
             'U2':  False, # Ventilador almoadilla
             'U3':  False, # Enfriamiento mecanico
             'U4':  False, # Calentador de aire  
             'U5':  False, # Sombreado externo (No hace nada por los parametros)
-            'U6_c':  False, # Respiraderos laterales
-            'U7_c':  True, # Ventilación forzada #Cambiamos U7 por U7_c para que U7 pudiera ser la solucion de un EDO
+            'U6_c':  True, # Respiraderos laterales
+            'U7_c':  False, # Ventilación forzada #Cambiamos U7 por U7_c para que U7 pudiera ser la solucion de un EDO
             'U8':  0.1, # Respiraderos del techo 
             'U9':  True, # Control sistema de niebla
             'U10': True, # Fuente externa de C02
