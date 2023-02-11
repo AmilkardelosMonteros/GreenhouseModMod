30ce9bfa19731c5b96ba3c0f94f5b64129e62f85
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2023-02-10 22:48:40 -0600

    Se quito la red

--- parameters/parameters_env.py
+++ parameters/parameters_env.py
@@ -35,9 +35,9 @@ PARAMS_ENV = {'n': n}
 #Para benchmark y tournament es recomendable que sea 'RANDOM', pero no absolutamente necesario.
 
 #El min de STEP  no es 1/24, pero el min de FRECUENCY SÍ es 60
-PARAMS_TRAIN = {'EPISODES': 1, 
+PARAMS_TRAIN = {'EPISODES': 900, 
                 'SPECIALIZATION_PERIOD': 0, 
-                'N_TEST': 1, #debe ser mayor o igual a 52  
+                'N_TEST': 300, #debe ser mayor o igual a 52  
                 'TYPE':'net', #puede ser net, bwn, unif
                 'STEPS':PARAMS_ENV['n'], \
                 'SHOW': False, \
@@ -45,8 +45,8 @@ PARAMS_TRAIN = {'EPISODES': 1,
                 'INDICE': 0, # Se usa en la simulacion al terminar el entrenamiento
                 'SAVE_FREQ': 1,
                 'SEND_MAIL':True,
-                'PATH_NET':'simulation_results/NuevoU7', #Must be None or a path(str)
-                'NET':'899' 
+                'PATH_NET':None, #Must be None or a path(str)
+                'NET':'' 
                 } 
 
 PARAMS_SIM = {'anio':2017,\
