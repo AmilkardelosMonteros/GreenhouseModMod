62ab05d441b9bc7f5ff9b376f042562f6f487441
AmilkardelosMonteros <amilkar@ciencias.unam.mx>
2024-03-20 21:02:44 -0600

    Se soluciono redundancia de parametros

--- greenhouse/crop/params_dias_tem.py
+++ greenhouse/crop/params_dias_tem.py
@@ -1,2 +1,7 @@
-DIAS = 90
+import os
+import sys   
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../")))
+from parameters.parameters_dir import PARAMS_DIR
+DIAS = PARAMS_DIR['days']
+
 
